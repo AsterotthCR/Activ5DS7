4d808d4df2b661d86b9333d8cfb25ef1b938c306
AsterotthCR <sheancordero97@gmail.com>
2024-10-15 18:29:37 -0500

    Database Config

---
+++ Config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Config;
+class Database {
+    private static $instance = null;
+    private $connection;
+
+    private function __construct() {
+        $this->connection = new PDO("mysql:host=localhost;dbname=classicmodels", "root", "contraseña");
+        $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public static function getInstance() {
+        if (!self::$instance) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
--- Models/User.php
+++ Models/User.php
@@ -1,73 +1,112 @@
 <?php
+
 class User {
-    private $id;
+    private $codUser;
     private $username;
     private $password;
-
-    // Constructor sin argumentos
-    public function __construct() {}
+    private $name;
+    private $lastname;
 
     // Constructor con todos los argumentos
-    public function __constructWithAll($id, $username, $password) {
-        $this->id = $id;
+    public function __construct($codUser = null, $username = null, $password = null, $name = null, $lastname = null) {
+        $this->codUser = $codUser;
         $this->username = $username;
         $this->password = $password;
+        $this->name = $name;
+        $this->lastname = $lastname;
     }
 
-    // Getters y Setters
-    public function getId() {
-        return $this->id;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
+    // Getters
+    public function getCodUser() {
+        return $this->codUser;
     }
 
     public function getUsername() {
         return $this->username;
     }
 
-    public function setUsername($username) {
-        $this->username = $username;
-    }
-
     public function getPassword() {
         return $this->password;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getLastname() {
+        return $this->lastname;
+    }
+
+    // Setters
+    public function setCodUser($codUser) {
+        $this->codUser = $codUser;
+    }
+
+    public function setUsername($username) {
+        $this->username = $username;
     }
 
     public function setPassword($password) {
         $this->password = $password;
     }
 
-    // Builder
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+    }
+
+    // Convertir a array
+    public function toArray() {
+        return [
+            'codUser' => $this->codUser,
+            'username' => $this->username,
+            'password' => $this->password,
+            'name' => $this->name,
+            'lastname' => $this->lastname
+        ];
+    }
+
+    // Builder pattern
     public static function builder() {
         return new class {
-            private $id;
+            private $codUser;
             private $username;
             private $password;
+            private $name;
+            private $lastname;
 
-            public function setId($id) {
-                $this->id = $id;
+            public function codUser($codUser) {
+                $this->codUser = $codUser;
                 return $this;
             }
 
-            public function setUsername($username) {
+            public function username($username) {
                 $this->username = $username;
                 return $this;
             }
 
-            public function setPassword($password) {
+            public function password($password) {
                 $this->password = $password;
                 return $this;
             }
 
+            public function name($name) {
+                $this->name = $name;
+                return $this;
+            }
+
+            public function lastname($lastname) {
+                $this->lastname = $lastname;
+                return $this;
+            }
+
             public function build() {
-                $user = new User();
-                $user->setId($this->id);
-                $user->setUsername($this->username);
-                $user->setPassword($this->password);
-                return $user;
+                return new User($this->codUser, $this->username, $this->password, $this->name, $this->lastname);
             }
         };
     }
 }
+
