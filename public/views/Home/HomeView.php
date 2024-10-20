e5b77fe533a96f834aa55dcf4b95bda7dcebad7c
AsterotthCR <sheancordero97@gmail.com>
2024-10-20 18:49:07 -0500

    chages in views locactions

--- public/views/Home/HomeView.php
+++ public/views/Home/HomeView.php
@@ -1,15 +1,32 @@
 <?php
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
     <title>Home</title>
-</head>
-<body>
-   <h1>Esto es la pagina home</h1>
-</body>
+
+    <link
+      rel="shortcut icon"
+      href="/assets/icons/icons8-zorro-48.png"
+      type="image/x-icon"
+    />
+    <link rel="stylesheet" href="public/assets/css/home.css">
+
+
+    <script
+      src="https://kit.fontawesome.com/ee26d7fa70.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <header>
+      <img class="logo" src="./assets/images/profile.png" alt="profile" title="profile" />
+      <navbar class="cabecera-menu">
+        <a href="#">Servicios</a>
+        <a href="#"><?php echo "@username"; ?></a>
+        <a href="#">Cerrar sesión</a>
+      </navbar>
+    </header>
+  </body>
 </html>
